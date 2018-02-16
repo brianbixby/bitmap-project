8f1bc4093a39b852d995210ddc21b5a0641e8d8d
Brian Bixby <brianbixby0@gmail.com>
2018-02-15 21:18:32 -0800

    able to create new image file

--- index.js
+++ index.js
@@ -6,23 +6,49 @@ const ee = new EE();
 const readFileHelper = require('./lib/file-reader.js');
 const transformFileHelper = require('./lib/file-transformer.js');
 const writeFileHelper = require('./lib/file-writer.js');
-const callback = require('./lib/callback.js');
 
-ee.on('readFileHelperDone', function(data) {
-  if(data) console.log('our data: ', data);
-  // if(files.length === 0) return;
-  // // removes last from array
-  // fs.readFile(files.pop(), function(err, data) {
-  //   if (err) return console.error(err);
-  //   ee.emit('filedone', data);
-  // });
-});
+let transformedFilePath = process.argv[3];
+let transformation = process.argv[4];
 
-readFileHelper(process.argv[2], callback);
+const wfCallback = function(err, data) {
+  if(err) throw err;
+  // console.log(data);
+};
+
+const tfCallback = function(err, data) {
+  if(err) throw err;
+  // console.log(data);
+  writeFileHelper(data, transformedFilePath, wfCallback);
+};
+
+// transformFileHelper(data, frCallback);
+
+const rfCallback = function(err, data) {
+  if(err) throw err;
+  // console.log(data);
+  transformFileHelper(data, transformation, tfCallback);
+};
+
+readFileHelper(process.argv[2], rfCallback);
+
+
+
+// var someCallback = function(data) {
+//   console.log('got some data: ', data);
+// }
+
+// var useCallback = function(cb) {
+//   cb('data that i want toget');
+// }
+
+// useCallback(someCallback);
+
+
 
 // node index.js original-file.bmp transformed-file.bmp transformation
 // process.argv[2] = original-file.bmp
 // process.argv[3] = transformed-file.bmp
 // process.argv[4] = transformation
-
 // node index.js palette-bitmap.bmp
+
+// node index.js palette-bitmap.bmp transformed-palette-bitmap.bmp invert

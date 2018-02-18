7f62313885aff5fccb8afe64bc44b257a4b625eb
Katy Robinson <katyorob@gmail.com>
2018-02-18 12:52:23 -0800

    transform module

--- __test__/file.transformer.test.js
+++ __test__/file.transformer.test.js
@@ -8,19 +8,21 @@ require('jest');
 
 
 describe('Transform Module', function(){
-    describe('invert', function(){
-        it('should invert the color table', function(done){
-            let testInvert = new BmData[0];
-            testInvert.pixelArray = [1, 2, 3];
-            testInvert.invert();
-            expect(testInvert.pixelArray).toEqual([3, 2, 1]);
-            done();
-        })
+  describe('invert', function(){
+    it('should invert the color table', function(done){
+      let testInvert = new BmData[0];
+      transformFileHelper('testInvert', function(){
+        testInvert.pixelArray = [1, 2, 3];
+        testInvert.invert();
+        expect(testInvert.pixelArray).toEqual([3, 2, 1]);
+        done();
+      });
     });
-    // describe('greyscale', function(){
-    //     it('should return the average of the numbers', function(done){
+  });
+  // describe('greyscale', function(){
+  //     it('should return the average of the numbers', function(done){
 
-    //         done();
-    //     })
-    // })
-})
+  //         done();
+  //     })
+  // })
+});

53fd3b5c2fc76b0d5a5cc9d581bb0d9ae80662d1
Katy Robinson <katyorob@gmail.com>
2018-02-18 16:03:02 -0800

    took data out of arguments for error test

--- __test__/file-transformer.test.js
+++ __test__/file-transformer.test.js
@@ -6,13 +6,13 @@ require('jest');
 describe('File Transformer Module', function() {
   describe('with improper file path or file type', function() {
     it('should return an error', function(done) {
-      fileTransformer('palette-bitmap.png', function(err, data) {
+      fileTransformer('palette-bitmap.png', function(err) {
         expect(err).toBeTruthy();
         expect(err.code).toEqual('ENOENT');
         // expect(err).toEqual('palette-bitmap.png is not a bmp file. Please visit https://online-converting.com/image/convert2bmp/ to convert this image to a 8-bit bmp file.');
         done();
       });
-      fileTransformer('notfound.bmp', function(err, data) {
+      fileTransformer('notfound.bmp', function(err) {
         expect(err).toBeTruthy();
         expect(err.code).toEqual('ENOENT');
         // expect(err).toEqual('Could not find notfound.bmp in assets folder. Please make sure it is there, check for typos and try again.');
